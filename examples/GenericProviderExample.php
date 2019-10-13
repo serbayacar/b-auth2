bad2f81d05f325afd637bb81d7eab71695381a16
Serbay ACAR <serbayacar@gmail.com>
2019-10-13 19:12:15 +0300

    Unneccesary files

--- examples/GenericProviderExample.php
+++ examples/GenericProviderExample.php
@@ -12,7 +12,8 @@ $interface->setURLs(['apiURL' => '##Provider api url comming is here##',
                            'tokenURL' => '##Provider auth token url comming is here##', 
                            'redirectURL' => '##Provider redirect uri comming is here##']);
 $interface->setScopes(['## If you need set scope, you write here ##']);
-//https://app.GenericProvider.com/oauth/authorize?client_id=58543c9b-7663-46e7-a586-327697996aed&redirect_uri=http://localhost:3000/examples/GenericProviderExample.php&scope=contacts
+
+
 $oauth = new Oauth2Client( $interface );
 
 if(!isset($_GET['code'])){
